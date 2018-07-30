3d75ae06a9cc17eaddc7cc3c698105e34deb0785
iMacTar <asd@www.org>
2018-07-30 19:05:01 +0300

    PS-2028.7

--- src/LiveControl/EloquentDataTable/EloquentUserProviderProvider.php
+++ src/LiveControl/EloquentDataTable/EloquentUserProviderProvider.php
@@ -1,4 +1,5 @@
 <?php namespace LiveControl\EloquentDataTable;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
 
 class EloquentUserProviderProvider extends \Illuminate\Auth\EloquentUserProvider
 {
